e7f579041e8c4c62d7cdb659720e55aef2016df2
akKLDMONOlald <161066696+akKLDMONOlald@users.noreply.github.com>
2025-04-15 21:26:35 +0200

    Update Serial.bat

--- Serial.bat
+++ Serial.bat
@@ -1,39 +1,19 @@
 @echo off
 cls
-mode con: cols=50 lines=35
-title Burgies | System Serials
-
-echo.
-echo [90m==============================================[97m
-echo             [96m[ SYSTEM INFORMATION ][97m
-echo [90m==============================================[97m
-
-echo.
-echo [93m[BIOS][97m
+mode con: cols=40 lines=31
+title Serials
+echo [90m==========================[97m
+echo Bios
 wmic bios get serialnumber
 wmic csproduct get uuid
-
-echo.
-echo [93m[CPU][97m
+echo CPU
 wmic cpu get serialnumber
 wmic cpu get processorid
-
-echo.
-echo [93m[Disk Drive][97m
+echo Diskdrive
 wmic diskdrive get serialnumber
-
-echo.
-echo [93m[Motherboard][97m
+echo Baseboard
 wmic baseboard get serialnumber
 wmic baseboard get manufacturer
-
-echo.
-echo [93m[Active Network MAC][97m
 wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress
-
-echo.
-echo [90m==============================================[97m
-echo               [92m[ PROCESS COMPLETED ][97m
-echo [90m==============================================[97m
-echo.
+echo [90m==========================[97m
 pause >nul
